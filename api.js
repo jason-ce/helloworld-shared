d25789bbd623fdb965007ebaaa9ea7797ca8edba
Jason <jason@phantomranch>
2017-03-06 13:34:15 -0500

    fixes

--- api.js
+++ api.js
@@ -1,22 +1,37 @@
-'use strict';
-
 var request = require('request');
 
 exports.User = class User {
 
-    getUserName(){
+    getUserName() {
         return "me";
     }
 };
 
-exports.Google = class Google{
-    constructor(){
-        console.log("constructor called");
-    }
-    getJSON(callback){        
-        let url = "https://inventory.data.gov/api/action/datastore_search?resource_id=d7c214da-cb07-4437-a6f4-ba89d1d9aca3&limit=5";
-        request(url, function(err, res, body) {              
-            callback(body);            
-        });
-    }
-};
+exports.Google = class Google {
+        constructor() {
+            console.log("constructor called");
+        }
+        getJSON(callback, url) {
+            //let url = "http://localhost:8000/study-schedule/api/v2/overview/?today=2017-03-06";
+            request(url, function (err, res, body) {
+                callback(body);
+                //console.log(body);           
+            });
+        }
+
+        // getJSON2(callback) {
+        //     var request = new Request('http://localhost:8000');
+
+        //     fetch("", {mode:'no-cors'})
+        //         .then(function (response) {
+        //             if (!response.ok) {
+        //                 throw Error(response.statusText);
+        //             }
+        //             return response;
+        //         }).then(function (response) {
+        //             console.log("ok");
+        //         }).catch(function (error) {
+        //             console.log(error);
+        //         });
+        // }
+    };
