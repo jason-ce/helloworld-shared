eab26e196a9125ce3653d53e4c50ca4fbd5e5318
Jason <jason@phantomranch>
2017-03-07 09:21:17 -0500

    change url to test s3

--- api.js
+++ api.js
@@ -2,7 +2,7 @@
 
 var request = require('request');
 
-exports.User = class User {
+exports.UserModel = class UserModel {
     getUserName() {
         return "me";
     }
@@ -13,15 +13,16 @@ exports.Books = class Books {
             console.log("constructor called");
         }
         getChapter(callback) {
-            let url = "http://localhost:8000";
+            let url = "http://www.google.com";
             //let url = "http://localhost:8000/study-schedule/api/v2/overview/?today=2017-03-06";
             request(url, function (err, res, body) {
                 callback(body);          
             });
         }
         getSection(callback) {
+            let url = "http://www.cramfighter.com";
             //let url = "http://localhost:8000";
-            let url = "http://localhost:8000/study-schedule/api/v2/overview/?today=2017-03-06";
+            //let url = "http://localhost:8000/study-schedule/api/v2/overview/?today=2017-03-06";
             request(url, function (err, res, body) {
                 callback(body);          
             });
